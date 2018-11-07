df56cf90ae67e1c351e628788afc6c787d6c8651
AN-SBP\allen <allennikka@gmail.com>
2018-11-06 16:11:26 -0800

    Simplified API request interface using request npm package

--- app.js
+++ app.js
@@ -1,82 +1,28 @@
 const express = require('express')
 const app = express()
-const https = require('https')
-const querystring = require('querystring')
+const request = require('request')
 const port = 3000
 
-// Set the configuration settings
-const credentials = {
-    client: {
-        id: 'allennikka',
-        secret: 'N3KEMfA0BJ'
-    },
-    auth: {
-        tokenHost: 'https://app1pub.smappee.net/dev/v1/oauth2/token'
-    }
-};
-
-function authReqPost(){
-    var form = {
+let options = {
+    url: 'https://app1pub.smappee.net/dev/v1/oauth2/token',
+    form: {
         grant_type: 'password',
         client_id: 'allennikka',
         client_secret: 'N3KEMfA0BJ',
         username: 'allennikka',
         password: 'Smap2Energy'
     }
-    
-    var postData = querystring.stringify(form);
-    var contentLength = postData.length;
-
-    var options = {
-        host: 'app1pub.smappee.net',
-        port: 443,
-        method: 'POST',
-        path: '/dev/v1/oauth2/token',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': contentLength
-        }
-    };
-
-    return [options, postData];
-}
-var crude_session = null;
-
-function postRequest([options, postData], output) {
-    // request option
-
-    var myreq = https.request(options, function (resp) {
-        var result = '';
-        resp.on('data', function (chunk) {
-            result += chunk;
-        });
-        resp.on('end', function () {
-            var result_json = JSON.parse(result);
-            output = result_json;
-        });
-        resp.on('error', function (err) {
-            console.log(err);
-        })
-    });
-
-    // req error
-    myreq.on('error', function (err) {
-        console.log(err);
-    });
-
-    //send request witht the postData form
-    myreq.write(postData);
-    myreq.end();
-}
-
-app.get('/getusers', function (req, res) {
-
-});
+};
 
 app.get('/', (req, res) => {
-    postRequest(authReqPost(), crude_session)
-    res.send('DONE!')
-
+    request.post(options, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        console.log('Upload successful!  Server responded with:', body);
+        res.send(body)
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
